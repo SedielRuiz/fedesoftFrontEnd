3010c17b203fb4b50d364e5f56e2b111018f7af7
dfer10 <didarragah@ucentral.edu.co>
2018-10-26 19:11:26 -0500

    icons

--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -21,5 +21,21 @@ export class PrincipalPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad PrincipalPage');
   }
-
+  slides = [
+    {
+      title: "Riesgos?",
+      description: "<b>Detalle</b> aqui van los riesgos khzcgvejnf smcmb bdsbc,  aadgkdxxhbw.cf ,jdgs...",
+      image: "assets/imgs/riesgos.png",
+    },
+    {
+      title: "Metodos",
+      description: "<b>Anticonceptivos</b> aqui van los metodos jasdiuscbukf jdghscisnd z,djgwsjnbc xjkesdnbv  j,wdgefd...",
+      image: "assets/imgs/metodosa.png",
+    },
+    {
+      title: "Causas",
+      description: "The <b>Cuales</b> Causas por las cuales se dan los embarazos.",
+      image: "assets/imgs/metodosa.png",
+    }
+    ];
 }
